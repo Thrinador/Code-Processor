fc9224b424042359ee7b770e963b1133e3bf4297
Thrinador <benjiclark@me.com>
2017-02-14 17:37:40 -0800

    Update JTextAreaNumbers.java
    Added method checkForNewWord which should start syntax highlighting. Needs work. also changed the field textArea from JTextArea to JTextPane. Finally added JAVA_KEY_WORDS string array that contains all of the java keywords.

--- JTextAreaNumbers.java
+++ JTextAreaNumbers.java
@@ -3,24 +3,54 @@ package code_processor;
 import java.awt.Color;
 import java.awt.Font;
 import static java.awt.Font.PLAIN;
-import javax.swing.JTextArea;
-import javax.swing.text.Element;
+import javax.swing.*;
+import javax.swing.text.*;
 
 /**
  * @author Ben
  * @author Polina
  */
-public class JTextAreaNumbers extends JTextArea {
+public class JTextAreaNumbers extends JTextPane {
 
-    private final JTextArea textArea;
+    private final JTextPane textArea;
+    private final String[] JAVA_KEY_WORDS = {"abstract", "continue", "for", "new",
+        "switch", "assert", "default", "goto", "package", "synchronized", 
+        "boolean", "do", "if", "private", "this", "break", "double",
+        "implements", "protected", "throw", "byte", "else", "import", 
+        "public", "throws", "case", "enum", "instanceof", "return", 
+        "transient", "catch", "extends", "int", "short", "try", "char", 
+        "final", "interface", "static", "void", "class", "finally", "long", 
+        "strictfp", "volatile", "const", "float", "native", "super", "while"};
 
-    public JTextAreaNumbers(JTextArea textArea) {
+    public JTextAreaNumbers(JTextPane textArea) {
         this.textArea = textArea;
         setBackground(Color.LIGHT_GRAY);
         setEditable(false);
         this.setText("1");
         this.setFont(new Font("Courier", PLAIN, 14));
 
+    }
+    
+    public void checkForNewWord(){
+        String[] words = textArea.getText().split(" ");
+        for(int i = 0; i < words.length; i++){
+            for(int j = 0; j < JAVA_KEY_WORDS.length; j++){
+                if(JAVA_KEY_WORDS[j].equals(words[i])){
+                    //Set color
+                    Document document = textArea.getDocument();
+                    //document.
+                    StyleContext context = new StyleContext();
+                    Style style = context.addStyle("test", null);
+                    StyleConstants.setForeground(style, Color.BLUE);
+                    try {
+                        document.insertString(0, words[i], style);
+                    } catch(BadLocationException e){
+                        System.err.print("Something went Wrong");
+                    }
+                    textArea.setDocument(document);
+                }
+            }
+        }
     }
 
     public void updateLineNumbers() {
